d27adcce734cba677a1d912f90c3709547f4f7dd
antoniofaical <antoniofaical06@gmail.com>
2025-07-29 15:01:13 -0300

    feature: index

---
+++ web/scripts/index.js
@@ -0,0 +1,12 @@
+async function verificarConfiguracoes() {
+    try {
+    const config = await eel.carregar_configuracoes()();
+    // Se carregar com sucesso, redireciona para login
+    window.location.href = "login.html";
+    } catch (e) {
+    // Se der erro (ex: FileNotFoundError), redireciona para cadastro
+    window.location.href = "cadastro.html";
+    }
+}
+
+verificarConfiguracoes();
